2f0ea1c56efcbecb47545b1f9fa02c3d79fa048a
Adegbe <67567108+Adegbe@users.noreply.github.com>
2025-06-22 22:17:54 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,23 @@
-<div className="p-10 bg-blue-100 text-center text-lg font-semibold text-blue-800">
-  Tailwind is working!
-</div>
+import '@/styles/globals.css'; // make sure Tailwind CSS is imported
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>MRCC EMR Preprocessing Tool</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      {/* Tailwind test block */}
+      <div className="p-10 bg-blue-100 text-center text-lg font-semibold text-blue-800">
+        Tailwind is working!
+      </div>
+
+      {/* Main page component */}
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
