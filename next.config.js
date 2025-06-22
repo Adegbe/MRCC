1369c7487f1671885c057533ee18c55e81e6db6e
Adegbe <67567108+Adegbe@users.noreply.github.com>
2025-06-22 19:54:32 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,20 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config) => {
-    // Add this to ignore the problematic package.json
+    // Add alias for components
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/components': path.resolve(__dirname, 'src/components'),
+      '@/services': path.resolve(__dirname, 'src/services')
+    };
+    
+    // Ignore package.json in services directory
     config.plugins.push(
       new (require('webpack').IgnorePlugin)({
         resourceRegExp: /\/src\/services\/package\.json$/
       })
     );
+    
     return config;
   }
 };
