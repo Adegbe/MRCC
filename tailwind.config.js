20dd5e611f741d7b9941ac5e0d5f3026d11b4140
Adegbe <67567108+Adegbe@users.noreply.github.com>
2025-06-22 17:59:23 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#121416',
+        secondary: '#6a7681',
+        accent: '#dce8f3',
+        background: '#f1f2f4',
+        border: '#dde1e3'
+      },
+    },
+  },
+  plugins: [],
+}
