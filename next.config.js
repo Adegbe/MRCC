04fbe259c48f0980bd20fe172a6dc2622f6155d9
Adegbe <67567108+Adegbe@users.noreply.github.com>
2025-06-22 19:19:46 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    serverActions: true,
-    optimizePackageImports: [
-      'js-sha256',
-      'papaparse',
-      'xlsx'
-    ]
-  },
-  
-  // Add this to ignore invalid package.json
   webpack: (config) => {
     config.plugins = config.plugins || [];
     config.plugins.push(new (require('webpack').IgnorePlugin)({
